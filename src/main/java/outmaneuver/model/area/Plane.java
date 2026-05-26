c5d28b4a40a2b62e98fe58cdf5b03f3932168e4f
Arthur Muller <arty97muller@gmail.com>
2026-05-27 01:22:00 +0200

    feat: add TurnState enum and Plane interface

---
+++ src/main/java/outmaneuver/model/area/Plane.java
@@ -0,0 +1,32 @@
+package outmaneuver.model.area;
+
+import outmaneuver.util.Vector2;
+
+public interface Plane {
+
+    Vector2 getPosition();
+
+    void setPosition(Vector2 position);
+
+    double getDirection();
+
+    void setDirection(double direction);
+
+    PlaneStats getStats();
+
+    void setStats(PlaneStats stats);
+
+    TurnState getTurnState();
+
+    void setTurnState(TurnState state);
+
+    boolean isShieldActive();
+
+    void activateShield();
+
+    void deactivateShield();
+
+    void applySpeedMultiplier(double factor, long durationMs);
+
+    double getEffectiveSpeed();
+}
