e9ef828783cda9b20b22314eb147fe82067d8c35
Arthur Muller <arty97muller@gmail.com>
2026-05-31 14:17:00 +0200

    feat: add Main entry point and wire application components

---
+++ src/main/java/outmaneuver/Main.java
@@ -0,0 +1,36 @@
+package outmaneuver;
+
+import outmaneuver.controller.impl.EntityControllerImpl;
+import outmaneuver.controller.impl.InputControllerImpl;
+import outmaneuver.controller.impl.MasterControllerImpl;
+import outmaneuver.model.area.Plane;
+import outmaneuver.model.area.PlaneImpl;
+import outmaneuver.model.area.StandardStats;
+import outmaneuver.view.swing.SwingGameView;
+
+public final class Main {
+
+    private static final double AREA_WIDTH = 800;
+    private static final double AREA_HEIGHT = 600;
+
+    private Main() {
+    }
+
+    public static void main(final String[] args) {
+        final Plane plane = new PlaneImpl(new StandardStats());
+        final InputControllerImpl inputCtrl = new InputControllerImpl();
+
+        final MasterControllerImpl[] masterRef = new MasterControllerImpl[1];
+        final EntityControllerImpl entityCtrl = new EntityControllerImpl(
+                plane, inputCtrl,
+                (evt, data) -> masterRef[0].onInternalEvent(evt, data),
+                AREA_WIDTH, AREA_HEIGHT);
+        final MasterControllerImpl master = new MasterControllerImpl(entityCtrl);
+        masterRef[0] = master;
+
+        final SwingGameView view = new SwingGameView(inputCtrl, master);
+        view.init();
+        master.attachView(view);
+        master.start();
+    }
+}
