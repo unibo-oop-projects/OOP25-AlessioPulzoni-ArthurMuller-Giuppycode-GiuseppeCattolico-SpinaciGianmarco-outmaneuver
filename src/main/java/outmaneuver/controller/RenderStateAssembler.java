a70520f9bec503dc7b4820b829f3fc60fcc3a660
Arthur Muller <arty97muller@gmail.com>
2026-06-24 16:41:12 +0200

    feat: update RenderStateAssembler signature

--- src/main/java/outmaneuver/controller/RenderStateAssembler.java
+++ src/main/java/outmaneuver/controller/RenderStateAssembler.java
@@ -7,8 +7,6 @@ import outmaneuver.view.RenderState;
 
 public interface RenderStateAssembler {
 
-    RenderState assemble(List<Entity> entities, boolean paused);
-
-    void reset();
+    RenderState assemble(List<Entity> entities, boolean paused,
+            long elapsedMs, int stars, double speedMultiplier, boolean shieldActive);
 }
-    
